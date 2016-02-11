d9ef773c5840bc0074bb5fb4b0e9acfd1a08a5d2
Chris Williams <cwilliams88@gmail.com>
2016-02-10 23:59:51 -0600

    more mission data

--- js/mission.js
+++ js/mission.js
@@ -3,6 +3,18 @@ var missionList = {
 		"back":  "Mission/AVP/A-Bounty-Protected-Back.jpg",
 		"front": "Mission/AVP/A-Bounty-Protected-Front.jpg"
 	},
+	"A New Threat":{
+		"back":  "Mission/Core/A-New-Threat-Back.jpg",
+		"front": "Mission/Core/A-New-Threat-Front.jpg"
+	},
+	"A Simple Task":{
+		"back":  "Mission/Core/A-Simple-Task-Back.jpg",
+		"front": "Mission/Core/A-Simple-Task-Front.jpg"
+	},
+	"Aftermath":{
+		"back":  "Mission/Core/Aftermath-Back.jpg",
+		"front": "Mission/Core/Aftermath-Front.jpg"
+	},
 	"Armed and Operational": {
 		"back":  "Mission/AVP/Armed-and-Operational-Back.jpg",
 		"front": "Mission/AVP/Armed-and-Operational-Front.jpg"
@@ -18,35 +30,172 @@ var missionList = {
 	"Brace for Impact": {
 		"back":  "Mission/AVP/Brace-For-Impact-Back.jpg",
 		"front": "Mission/AVP/Brace-For-Impact-Front.jpg"
+	},
+	"Breaking Point":{
+		"back":  "Mission/Core/Breaking-Point-Back.jpg",
+		"front": "Mission/Core/Breaking-Point-Front.jpg"
+	},
+	"Brushfire":{
+		"back":  "Mission/Core/Brushfire-Back.jpg",
+		"front": "Mission/Core/Brushfire-Front.jpg"
+	},
+	"Brute Force": {
+		"back":  "Mission/AVP/Brute-Force-Back.jpg",
+		"front": "Mission/AVP/Brute-Force-Front.jpg"
+	},
+	"Bunker Buster": {
+		"back":  "Mission/AVP/Bunker-Buster-Back.jpg",
+		"front": "Mission/AVP/Bunker-Buster-Front.jpg"
+	},
+	"Captured":{
+		"back":  "Mission/Core/Captured-Back.jpg",
+		"front": "Mission/Core/Captured-Front.jpg"
+	},
+	"Celebration": {
+		"back":  "Mission/AVP/Celebration-Back.jpg",
+		"front": "Mission/AVP/Celebration-Front"
+	},
+	"Chain of Command":{
+		"back":  "Mission/Core/Chain-Of-Command-Back.jpg",
+		"front": "Mission/Core/Chain-Of-Command-Front.jpg"
+	},
+	"Communication Breakdown": {
+		"back":  "Mission/AVP/Communication-Breakdown-Back.jpg",
+		"front": "Mission/AVP/Communication-Breakdown-Front.jpg"
+	},
+	"Dark Obsession":{
+		"back":  "Mission/Core/Dark-Obsession-Back.jpg",
+		"front": "Mission/Core/Dark-Obsession-Front.jpg"
+	},
+	"Desperate Hour":{
+		"back":  "Mission/Core/Desperate-Hour-Back.jpg",
+		"front": "Mission/Core/Desperate-Hour-Front.jpg"
+	},
+	"Drawn In":{
+		"back":  "Mission/Core/Drawn-In-Back.jpg",
+		"front": "Mission/Core/Drawn-In-Front.jpg"
+	},
+	"Fly Solo":{
+		"back":  "Mission/Core/Fly-Solo-Back.jpg",
+		"front": "Mission/Core/Fly-Solo-Front.jpg"
+	},
+	"Forest Ambush": {
+		"back":  "Mission/AVP/Forest-Ambush-Back.jpg",
+		"front": "Mission/AVP/Forest-Ambush-Front.jpg"
+	},
+	"Friends of Old":{
+		"back":  "Mission/Core/Friends-of-Old-Back.jpg",
+		"front": "Mission/Core/Friends-of-Old-Front.jpg"
+	},
+	"Fully Charged": {
+		"back":  "Mission/AVP/Fully-Charged-Back.jpg",
+		"front": "Mission/AVP/Forest-Ambush-Front.jpg"
+	},
+	"Generous Donations":{
+		"back":  "Mission/Core/Generous-Donations-Back.jpg",
+		"front": "Mission/Core/Generous-Donations-Front.jpg"
+	},
+	"High Moon":{
+		"back":  "Mission/Core/High-Moon-Back.jpg",
+		"front": "Mission/Core/High-Moon-Front.jpg"
+	},
+	"Homecoming":{
+		"back":  "Mission/Core/Homecoming-Back.jpg",
+		"front": "Mission/Core/Homecoming-Front.jpg"
+	},
+	"Imperial Entanglements": {
+		"back":  "Mission/AVP/Imperial-Entanglements-Back.jpg",
+		"front": "Mission/AVP/Imperial-Entanglements-Front.jpg"
+	},
+	"Imperial Hospitality":{
+		"back":  "Mission/Core/Imperial-Hospitality-Back.jpg",
+		"front": "Mission/Core/Imperial-Hospitality-Front.jpg"
+	},
+	"Impounded":{
+		"back":  "Mission/Core/Impounded-Back.jpg",
+		"front": "Mission/Core/Impounded-Front.jpg"
+	},
+	"Incoming":{
+		"back":  "Mission/Core/Incoming-Back.jpg",
+		"front": "Mission/Core/Incoming-Front.jpg"
+	},
+	"Indebted":{
+		"back":  "Mission/Core/Indebted-Back.jpg",
+		"front": "Mission/Core/Indebted-Front.jpg"
+	},
+	"Infection": {
+		"back":  "Mission/AVP/Infection-Back.jpg",
+		"front": "Mission/AVP/Infection-Front.jpg"
+	},
+	"Last Stand":{
+		"back":  "Mission/Core/Last-Stand-Back.jpg",
+		"front": "Mission/Core/Last-Stand-Front.jpg"
+	},
+	"Loose Cannon":{
+		"back":  "Mission/Core/Loose-Cannon-Back.jpg",
+		"front": "Mission/Core/Loose-Cannon-Front.jpg"
+	},
+	"Luxury Cruise":{
+		"back":  "Mission/Core/Luxury-Cruise-Back.jpg",
+		"front": "Mission/Core/Luxury-Cruise-Front.jpg"
+	},
+	"Means of Production":{
+		"back":  "Mission/Core/Means-of-Production-Back.jpg",
+		"front": "Mission/Core/Means-of-Production-Front.jpg"
+	},
+	"Open To Interpretation": {
+		"back":  "Mission/AVP/Open-to-Interpretation-Back.jpg",
+		"front": "Mission/AVP/Open-to-Interpretation-Front.jpg"
+	},
+	"Predator and Prey": {
+		"back":  "Mission/AVP/Predator-and-Prey-Back.jpg",
+		"front": "Mission/AVP/Predator-and-Prey-Front.jpg"
+	},
+	"Snowcrash": {
+		"back":  "Mission/AVP/Snowcrash-Back.jpg",
+		"front": "Mission/AVP/Snowcrash-Front.jpg"
+	},
+	"Sorry About the Mess":{
+		"back":  "Mission/Core/Sorry-About-The-Mess-Back.jpg",
+		"front": "Mission/Core/Sorry-About-The-Mess-Front.jpg"
+	},
+	"Strength of Command": {
+		"back":  "Mission/AVP/Strength-of-Command-Back.jpg",
+		"front": "Mission/AVP/Strength-of-Command-Front.jpg"
+	},
+	"Sympathy for the Rebellion":{
+		"back":  "Mission/Core/Sympathy-For-The-Rebellion-Back.jpg",
+		"front": "Mission/Core/Sympathy-For-The-Rebellion-Front.jpg"
+	},
+	"Target of Opportunity":{
+		"back":  "Mission/Core/Target-Of-Opportunity-Back.jpg",
+		"front": "Mission/Core/Target-Of-Opportunity-Front.jpg"
+	},
+	"Temptation":{
+		"back":  "Mission/Core/Temptation-Back.jpg",
+		"front": "Mission/Core/Temptation-Front.jpg"
+	},
+	"The Source":{
+		"back":  "Mission/Core/The-Source-Back.jpg",
+		"front": "Mission/Core/The-Source-Front.jpg"
+	},
+	"The Spice Job":{
+		"back":  "Mission/Core/The-Spice-Job-Back.jpg",
+		"front": "Mission/Core/The-Spice-Job-Front.jpg"
+	},
+	"Under Siege":{
+		"back":  "Mission/Core/Under-Siege-Back.jpg",
+		"front": "Mission/Core/Under-Siege-Front.jpg"
+	},
+	"Viper's Den":{
+		"back":  "Mission/Core/Vipers-Den-Back.jpg",
+		"front": "Mission/Core/Vipers-Den-Front.jpg"
+	},
+	"Wanted":{
+		"back":  "Mission/Core/Wanted-Back.jpg",
+		"front": "Mission/Core/Wanted-Front.jpg"
 	}
-
 }
-/*
-	"Brute-Force-Back.jpg",
-	"Brute-Force-Front.jpg",
-	"Bunker-Buster-Back.jpg",
-	"Bunker-Buster-Front.jpg",
-	"Celebration-Back.jpg",
-	"Celebration-Front.jpg",
-	"Communication-Breakdown-Back.jpg",
-	"Communication-Breakdown-Front.jpg",
-	"Forest-Ambush-Back.jpg",
-	"Forest-Ambush-Front.jpg",
-	"Fully-Charged-Back.jpg",
-	"Fully-Charged-Front.jpg",
-	"Imperial-Entanglements-Back.jpg",
-	"Imperial-Entanglements-Front.jpg",
-	"Infection-Back.jpg",
-	"Infection-Front.jpg",
-	"Open-to-Interpretation-Back.jpg",
-	"Open-to-Interpretation-Front.jpg",
-	"Predator-and-Prey-Back.jpg",
-	"Predator-and-Prey-Front.jpg",
-	"Snowcrash-Back.jpg",
-	"Snowcrash-Front.jpg",
-	"Strength-of-Command-Back.jpg",
-	"Strength-of-Command-Front.jpg"
- */
 
 $(function() {
 	var availableTags = Object.keys(missionList);
@@ -55,11 +204,8 @@ $(function() {
 		select: function( event, ui ) {
 			if(ui.item){
 				var missionName = ui.item.value;
-				console.log(missionName);
 				var missionCard = missionList[missionName];
-				console.log(JSON.stringify(missionCard));
 				$('#mission-card-front').attr('src', imgRoot + missionCard.front);
-				console.log(JSON.stringify(missionCard.front));
 				$('.flip-container').slideDown();
 				$('#mission-card-back').attr('src', imgRoot + missionCard.back);
 
